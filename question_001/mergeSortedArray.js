9b079de4aff50297d0fbb8cd9bb4ff8370b59220
StevenYangSX <yangshixin1206@gmail.com>
2024-03-09 16:48:57 -0800

    Understand Question_001.

--- question_001/mergeSortedArray.js
+++ question_001/mergeSortedArray.js
@@ -13,20 +13,26 @@
 // 3. optimal solution: starting pionters from the back of arr1 and arr2. save space
 
 var mergeSortedArray = function (nums1, m, nums2, n) {
-  let i = m - 1;
-  let j = n - 1;
-  let k = m + n - 1;
+  let tail = m + n - 1;
+  let p1 = m - 1;
+  let p2 = n - 1;
 
-  while (j >= 0) {
-    if (i >= 0 && nums1[i] > nums2[j]) {
-      nums1[k] = nums1[i];
-      k--;
-      i--;
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] >= nums2[p2]) {
+      nums1[tail] = nums1[p1];
+      p1--;
     } else {
-      nums1[k] = nums2[j];
-      k--;
-      j--;
+      nums1[tail] = nums2[p2];
+      p2--;
     }
+    tail--;
+  }
+
+  // after the above loop, if p2 is still >=0, there are elements in nums2 that needs to be copied to nums1
+  while (p2 >= 0) {
+    nums1[tail] = nums2[p2];
+    p2--;
+    tail--;
   }
 };
 
